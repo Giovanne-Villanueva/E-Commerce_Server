3f08c22069c8a0bcc958144c8322485832e83055
Giovanne-Villanueva <gvv8106@gmail.com>
2023-06-15 12:07:42 -0700

    added models for project and built index model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  // When we delete a Reader, make sure to also delete the associated Library Card.
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product,{
+  through: ProductTag,
+})
 
 module.exports = {
   Product,
